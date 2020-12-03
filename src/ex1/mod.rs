20950970d364dd4d32dbfc70e80c0b0fbc44bdae
Mike <mike@weberapps.com>
2020-12-02 18:43:03 -0600

    Solve day 1, part 2

--- src/ex1/mod.rs
+++ src/ex1/mod.rs
@@ -1,48 +1,94 @@
-use std::collections::HashMap;
 use crate::utils::*;
 
-pub fn part_a(filename: Option<&String>) -> u32 {
+const TARGET_VALUE: usize = 2020;
+
+pub fn part_a(filename: Option<&String>) -> Option<u32> {
     let report_values = match filename {
         Some(path) => parse_report(path),
         None => {
             println!("Please specify which file you'd like to run");
-            return 0;
+            return None;
         }
     };
-    return find_value(2020, &report_values);
+    return find_value(TARGET_VALUE, &report_values);
 }
 
-fn parse_report(filename: &String) -> HashMap<u32, u32> {
-    let mut map = HashMap::new();
+pub fn part_b(filename: Option<&String>) -> Option<u32> {
+    let report_values = match filename {
+        Some(path) => parse_report(path),
+        None => {
+            println!("Please specify which file you'd like to run");
+            return None;
+        }
+    };
+    return find_triple_value(TARGET_VALUE, &report_values);
+}
+
+fn parse_report(filename: &String) -> [bool; TARGET_VALUE] {
+    let mut expense_reports: [bool; TARGET_VALUE] = [false; TARGET_VALUE];
     if let Ok(lines) = read_lines(filename) {
         for line in lines {
             if let Ok(r) = line {
-                let val = r.parse::<u32>().unwrap();
-                map.insert(val, val);
+                let val = r.parse::<usize>().unwrap();
+                expense_reports[val] = true;
             }
         }
     }
 
-    return map;
+    return expense_reports;
 }
 
-fn find_value(target_value: u32, report_values: &HashMap<u32, u32>) -> u32 {
-    for (value, _) in report_values {
-        let diff = target_value - value;
-        if let Some(other_value) = report_values.get(&diff) {
-            return other_value * value;
+fn find_triple_value(target_value: usize, report_values: &[bool; TARGET_VALUE]) -> Option<u32> {
+    for (i, in_list1) in report_values.iter().enumerate() {
+        if *in_list1 {
+            for (j, in_list2) in report_values[(i + 1)..report_values.len()].iter().enumerate() {
+                if *in_list2 {
+                    // We're looping through a sublist, so the enumerator is going to
+                    // start over at an index of 0. Add back the fqct that we're starting
+                    // at an index of `i + 1`.
+                    let val2 = j + i + 1;
+                    let diff = target_value - (i + val2);
+                    if report_values[diff] {
+                        return Some((i * val2 * diff) as u32);
+                    }
+                }
+            }
+        }
+    }
+    return None;
+}
+
+fn find_value(target_value: usize, report_values: &[bool; TARGET_VALUE]) -> Option<u32> {
+    for (i, in_list) in report_values.iter().enumerate() {
+        if *in_list {
+            let diff = target_value - i;
+            if report_values[diff] {
+                return Some((i * diff) as u32);
+            }
         }
     }
 
-    return 0;
+    return None;
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn with_sample() {
-        let result = crate::ex1::part_a(Some(&"./src/ex1/sample.txt".to_string()));
-        assert_eq!(result, 514579);
+    fn test_part_a() {
+        if let Some(result) = crate::ex1::part_a(Some(&"./src/ex1/sample.txt".to_string())) {
+            assert_eq!(result, 514579);
+        } else {
+            panic!("Could not find a match");
+        }
+    }
+
+    #[test]
+    fn test_part_b() {
+        if let Some(result) = crate::ex1::part_b(Some(&"./src/ex1/sample.txt".to_string())) {
+            assert_eq!(result, 241861950);
+        } else {
+            panic!("Could not find a match");
+        }
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,17 @@ fn main() {
         let prog_num = args[1].as_str();
         let result = match prog_num {
             "1a" => ex1::part_a(args.get(2)),
+            "1b" => ex1::part_b(args.get(2)),
             _ => {
                 println!("Could not recognize program {}", prog_num);
-                0
+                None
             }
         };
-        println!("The result is {}", result);
+        if let Some(answer) = result {
+            println!("The result is {}", answer);
+        } else {
+            println!("No solution found for program {}", prog_num);
+        }
     } else {
         println!("No program specified");
     }
