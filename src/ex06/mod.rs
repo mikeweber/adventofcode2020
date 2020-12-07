e2a1f37f6c128c31a4cfa90afed0ea41c54840b3
Mike <mike@weberapps.com>
2020-12-07 13:54:56 -0600

    Solve Day 6, part 1

--- src/ex06/mod.rs
+++ src/ex06/mod.rs
@@ -1,27 +1,44 @@
-use crate::utils::*;
+use std::fs;
 
 pub fn part_a(filename: Option<&String>) -> Option<u32> {
-    None
+    match filename {
+        Some(path) => Some(sum_groups(parse_groups(fs::read_to_string(path).unwrap()))),
+        None => None
+    }
+}
+
+fn sum_groups(groups: Vec<u32>) -> u32 {
+    groups.iter().fold(0, |count, sum| count + sum)
 }
 
 fn parse_groups(group_str: String) -> Vec<u32> {
     let mut new_line = false;
     let mut groups = vec![];
     let mut cur_group = vec![];
+    let mut sum = 0;
 
     for ch in group_str.chars() {
         if ch == '\n' {
             if new_line {
                 groups.push(cur_group.len() as u32);
+                sum += cur_group.len() as u32;
+                println!("cur_group ({}/{}): {:?}", cur_group.len(), sum, cur_group);
                 cur_group.clear();
                 new_line = false;
             } else {
                 new_line = true;
             }
-        } else if !cur_group.contains(&ch) {
-            cur_group.push(ch);
+        } else {
             new_line = false;
+            if !cur_group.contains(&ch) {
+                cur_group.push(ch);
+                cur_group.sort();
+                new_line = false;
+            }
         }
+    }
+    if !cur_group.is_empty() {
+        groups.push(cur_group.len() as u32);
     }
     return groups;
 }
